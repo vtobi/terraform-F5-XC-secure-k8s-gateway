5956f66eb0a89de212a16350bfdd7fea4e09b03c
vtobi <97698702+vtobi@users.noreply.github.com>
2022-06-21 16:25:52 +0100

    Update loadbalancer.tf

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -33,43 +33,43 @@ resource "volterra_namespace" "this" {
 #  endpoint_selection = "LOCAL_PREFERRED"
 #}
 
-resource "volterra_app_firewall" "this" {
-  for_each                 = toset(var.eks_only ? [] : [var.skg_name])
-  name                     = format("%s-waf", var.skg_name)
-  description              = format("WAF in block mode for %s", var.skg_name)
-  namespace                = local.namespace
-  allow_all_response_codes = true
-}
+#resource "volterra_app_firewall" "this" {
+#  for_each                 = toset(var.eks_only ? [] : [var.skg_name])
+#  name                     = format("%s-waf", var.skg_name)
+#  description              = format("WAF in block mode for %s", var.skg_name)
+#  namespace                = local.namespace
+#  allow_all_response_codes = true
+#}
 
-resource "volterra_http_loadbalancer" "this" {
-  for_each                        = toset(var.eks_only ? [] : [var.skg_name])
-  name                            = format("%s-lb", var.skg_name)
-  namespace                       = local.namespace
-  description                     = format("HTTPS loadbalancer object for %s origin server", var.skg_name)
-  domains                         = [var.app_domain]
-  advertise_on_public_default_vip = true
-  default_route_pools {
-    pool {
-      name      = volterra_origin_pool.this[each.key].name
-      namespace = local.namespace
-    }
-  }
-  https_auto_cert {
-    add_hsts      = var.enable_hsts
-    http_redirect = var.enable_redirect
-    no_mtls       = true
-  }
-  app_firewall {
-    name      = volterra_app_firewall.this[each.key].name
-    namespace = local.namespace
-  }
-  disable_waf                     = false
-  disable_rate_limit              = true
-  round_robin                     = true
-  service_policies_from_namespace = true
-  no_challenge                    = false
-  js_challenge {
-    js_script_delay = var.js_script_delay
-    cookie_expiry   = var.js_cookie_expiry
-  }
-}
+#resource "volterra_http_loadbalancer" "this" {
+#  for_each                        = toset(var.eks_only ? [] : [var.skg_name])
+#  name                            = format("%s-lb", var.skg_name)
+#  namespace                       = local.namespace
+#  description                     = format("HTTPS loadbalancer object for %s origin server", var.skg_name)
+#  domains                         = [var.app_domain]
+#  advertise_on_public_default_vip = true
+#  default_route_pools {
+#    pool {
+#      name      = volterra_origin_pool.this[each.key].name
+#      namespace = local.namespace
+#    }
+#  }
+#  https_auto_cert {
+#    add_hsts      = var.enable_hsts
+#    http_redirect = var.enable_redirect
+#    no_mtls       = true
+#  }
+#  app_firewall {
+#    name      = volterra_app_firewall.this[each.key].name
+#    namespace = local.namespace
+#  }
+#  disable_waf                     = false
+#  disable_rate_limit              = true
+#  round_robin                     = true
+#  service_policies_from_namespace = true
+#  no_challenge                    = false
+#  js_challenge {
+#    js_script_delay = var.js_script_delay
+#    cookie_expiry   = var.js_cookie_expiry
+#  }
+#}
