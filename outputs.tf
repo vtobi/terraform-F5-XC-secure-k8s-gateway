59cfb828260a7776a6acead56af99f1122b46992
vtobi <97698702+vtobi@users.noreply.github.com>
2022-06-22 18:08:56 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 output "kubeconfig_filename" {
   description = "EKS kubeconfig file name"
-  value       = module.eks.kubeconfig_filename
+  value       = local_file.this_kubeconfig.filename
 }
 
 output "f5_xc_aws_vpc_site" {
